0ff522ee8fe84373504bd2e579d79489a1e734ca
Wolfgang Mathurin <wmathurin@salesforce.com>
2025-07-03 16:24:00 -0600

    Moving to cordova android 14.0.1

--- shared/constants.js
+++ shared/constants.js
@@ -65,7 +65,7 @@ module.exports = {
 //             pluginRepoUri: 'salesforce-mobilesdk-cordova-plugin@v' + VERSION, // GA
             platformVersion: {
                 ios: '7.1.1',
-                android: '13.0.0'
+                android: '14.0.1'
             }
         },
         sf: {
